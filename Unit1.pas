66c9cb8aa93828e6bd1c270e59905afc4100dace
menshikovartem <menshikov.artyom1999@yandex.ru>
2019-12-26 19:17:41 +0300

    Начало книжного алгоритма

--- Unit1.pas
+++ Unit1.pas
@@ -32,8 +32,12 @@ var
   otr, pr, otr1: boolean;
   i: Integer;
   x1, y1, x2, y2, xr1, xr2, yr1, yr2: Integer;
-  xpr1, ypr1, xpr2, ypr2, xl1, xl2, xl3, xl4, yl1, yl2, yl3, yl4: Integer;
+  xle, xr, yt, yb, xpr1, ypr1, xpr2, ypr2, xl1, xl2, xl3, xl4, yl1, yl2, yl3,
+    yl4: Integer;
   k, b: real;
+
+  T1code: array [1 .. 4] of Integer;
+  T2code: array [1 .. 4] of Integer;
 
 implementation
 
@@ -94,50 +98,45 @@ begin
   Showmessage('Очистка выполнена!'); // Поясняющий комментарий для пользователя
 end;
 
+Function Min(a, b: Integer): Integer;
+begin
+
+  if b <= a then
+    result := b
+  else
+    result := a;
+
+end;
+
+Function Max(a, b: Integer): Integer;
+begin
+
+  if b >= a then
+    result := b
+  else
+    result := a;
+
+end;
+
+function BoolToInt(f: boolean): Integer;
+begin
+
+  if f then
+    result := 1
+  else
+    result := 0;
+
+end;
+
 procedure TForm1.Button3Click(Sender: TObject); // Процедура отсечения
 
 var
   p1, p2: byte;
-  x0, y0: Integer;
+  x0, y0, x1new, x2new, y1new, y2new,sum1,sum2: Integer;
   stop: boolean;
   FLAG, flag2, FlagFirstIn, FlagSecondIn: boolean;
   S, S2: STRiNG;
-
-  // function MoveToStart(xl, Yl, xpr, ypr: Integer): string;
-  // begin
-  //
-  // result := false;
-  //
-  // if (((xpr - 1 { погрешность } ) <= xl) and (xl <= (xpr + 1 { погрешность } )
-  // ) and (Yl = ypr)) then
-  // begin
-  // result := true;
-  // end
-  //
-  // else if (((xpr1 - 1 { погрешность } ) <= xl2) and
-  // (xl2 <= (xpr2 + 1 { погрешность } )) and (yl2 = ypr1)) then
-  // begin
-  // result := true;
-  // end
-  //
-  // else if (((xpr1 - 1 { погрешность } ) <= xl3) and
-  // (xl3 <= (xpr2 + 1 { погрешность } )) and (yl3 = ypr1)) then
-  // begin
-  // result := true;
-  // end
-  //
-  // else if (((xpr1 - 1 { погрешность } ) <= xl4) and
-  // (xl4 <= (xpr2 + 1 { погрешность } )) and (yl4 = ypr1)) then
-  // begin
-  // result := true;
-  // end;
-  //
-  // end;
-  //
-  // procedure LineToEnd();
-  // begin
-  //
-  // end;
+  label l1,l2,l3,l4,l5,l6,l7,l8;
 
 begin
 
@@ -148,410 +147,43 @@ begin
   k := (y1 - y2) / (x1 - x2);
   b := y2 - k * x2;
 
-  // пересечение с левой линией
+  // (x1,y1)(x2,y2)-координаты отрезка
+  // (xpr1,ypr1)(xpr2,ypr2) -
 
-  FlagFirstIn := false;
-  FlagSecondIn := false;
+  xle := Min(xpr1, xpr2);
+  xr := Max(xpr1, xpr2);
+  yt := Min(ypr1, ypr2);
+  yb := Max(ypr1, ypr2);
 
-  if (xpr1 < x1) and (x1 < xpr2) and (ypr1 < y1) and (y1 < ypr2) then
-  begin
-    xl1 := x1;
-    yl1 := y1;
-    FlagFirstIn := true;
-  end
-  else
-  begin
-    xl1 := xpr1;
-    yl1 := round(k * xl1 + b);
-  end;
+  T1code[4] := BoolToInt(x1 < xle);
+  T1code[3] := BoolToInt(x1 > xr);
+  T1code[2] := BoolToInt(y1 > yb);
+  T1code[1] := BoolToInt(y1 < yt);
 
-  if (xpr1 < x2) and (x2 < xpr2) and (ypr1 < y2) and (y2 < ypr2) then
-  begin
-    xl2 := x2;
-    yl2 := y2;
-    FlagSecondIn := true;
-  end
-  else
-  begin
-    xl2 := xpr2;
-    yl2 := round(k * xl2 + b);
-  end;
+  T2code[4] := BoolToInt(x2 < xle);
+  T2code[3] := BoolToInt(x2 > xr);
+  T2code[2] := BoolToInt(y2 > yb);
+  T2code[1] := BoolToInt(y2 < yt);
 
+  FLAG := false;
+  x1new := x1;
+  x2new := x2;
+  y1new := y1;
+  y2new := y2;
+  k := high(xr); // максимальное число типа переменной xr т е инта
 
+  sum1:=0;
+  sum2:=0;
 
-
-  // пересечение с правой
-
-  // пересечение с верхней линией
-  yl3 := ypr1;
-  xl3 := round((yl3 - b) / k);
-
-  // пересечение с нижней линией
-
-  yl4 := ypr2;
-  xl4 := round((yl4 - b) / k);
-
-  // определение по каким точкам отсекать
-  // начальные точки отрезка начало
-
-  FLAG := true;
-  flag2 := true;
-
-  if (((xpr1 - 1 { погрешность } ) <= xl1) and
-    (xl1 <= (xpr2 + 1 { погрешность } )) and ((yl1 = ypr1)or ((ypr1 < yl1) and (yl1 < ypr2)))) then
-  begin
-    if FLAG then
+  for i  := 1 to 4 do
     begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl1, yl1);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl1, yl1);
+      sum1:=sum1 +  T1code[i];
+      sum2:=sum2 +  T2code[i];
     end;
 
-  end
+  if (sum1 = 0) and (sum2 = 0) then 
+  
 
-  else if (((xpr1 - 1 { погрешность } ) <= xl2) and
-    (xl2 <= (xpr2 + 1 { погрешность } )) and ((yl2 = ypr1)or ((ypr1 < yl2) and (yl2 < ypr2)))) then
-  begin
-
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl2, yl2);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl2, yl2);
-    end;
-
-  end
-
-  else if (((xpr1 - 1 { погрешность } ) <= xl3) and
-    (xl3 <= (xpr2 + 1 { погрешность } )) and ((yl3 = ypr1)or ((ypr1 < yl3) and (yl3 < ypr2)))) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl3, yl3);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl3, yl3);
-    end;
-  end
-
-  else if (((xpr1 - 1 { погрешность } ) <= xl4) and
-    (xl4 <= (xpr2 + 1 { погрешность } )) and ((yl4 = ypr1)or ((ypr1 < yl4) and (yl4 < ypr2)))) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl4, yl4);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl4, yl4);
-    end;
-  end;
-
-  if (((xpr1 - 1 { погрешность } ) <= xl1) and
-    (xl1 <= (xpr2 + 1 { погрешность } )) and ((yl1 = ypr1)or ((ypr1 < yl1) and (yl1 < ypr2)))) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl1, yl1);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl1, yl1);
-    end;
-
-  end
-
-  else if (((xpr1 - 1 { погрешность } ) <= xl2) and
-    (xl2 <= (xpr2 + 1 { погрешность } )) and ((yl2 = ypr1)or ((ypr1 < yl2) and (yl2 < ypr2)))) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl2, yl2);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl2, yl2);
-    end;
-  end
-
-  else if (((xpr1 - 1 { погрешность } ) <= xl3) and
-    (xl3 <= (xpr2 + 1 { погрешность } )) and ((yl3 = ypr1)or ((ypr1 < yl3) and (yl3 < ypr2)))) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl3, yl3);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl3, yl3);
-    end;
-  end
-
-  else if (((xpr1 - 1 { погрешность } ) <= xl4) and
-    (xl4 <= (xpr2 + 1 { погрешность } )) and ((yl4 = ypr1)or ((ypr1 < yl4) and (yl4 < ypr2)))) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl4, yl4);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl4, yl4);
-    end;
-  end;
-
-  if (((ypr1 - 1 { погрешность } ) <= yl1) and
-    (yl1 <= (ypr2 + 1 { погрешность } )) and ((xl1 = xpr1)or ((xpr1 < xl1) and (xl1 < xpr2)) )) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl1, yl1);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl1, yl1);
-    end;
-
-  end
-
-  else if (((ypr1 - 1 { погрешность } ) <= yl2) and
-    (yl2 <= (ypr2 + 1 { погрешность } )) and ((xl2 = xpr1)or ((xpr1 < xl2) and (xl2 < xpr2)) )) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl2, yl2);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl2, yl2);
-    end;
-  end
-
-  else if (((ypr1 - 1 { погрешность } ) <= yl3) and
-    (yl3 <= (ypr2 + 1 { погрешность } )) and ((xl3 = xpr1)or ((xpr1 < xl3) and (xl3 < xpr2)) )) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl3, yl3);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl3, yl3);
-    end;
-  end
-
-  else if (((ypr1 - 1 { погрешность } ) <= yl4) and
-    (yl4 <= (ypr2 + 1 { погрешность } )) and ((xl4 = xpr1)or ((xpr1 < xl4) and (xl4 < xpr2)) )) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl4, yl4);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl4, yl4);
-    end;;
-  end;
-
-  if (((ypr1 - 1 { погрешность } ) <= yl1) and
-    (yl1 <= (ypr2 + 1 { погрешность } )) and ((xl1 = xpr1)or ((xpr1 < xl1) and (xl1 < xpr2)) )) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl1, yl1);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl1, yl1);
-    end;
-  end
-
-  else if (((ypr1 - 1 { погрешность } ) <= yl2) and
-    (yl2 <= (ypr2 + 1 { погрешность } )) and ((xl2 = xpr1)or ((xpr1 < xl2) and (xl2 < xpr2)) )) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl2, yl2);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl2, yl2);
-    end;
-  end
-
-  else if (((ypr1 - 1 { погрешность } ) <= yl3) and
-    (yl3 <= (ypr2 + 1 { погрешность } )) and ((xl3 = xpr1)or ((xpr1 < xl3) and (xl3 < xpr2)) )) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl3, yl3);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl3, yl3);
-    end;
-  end
-
-  else if (((ypr1 - 1 { погрешность } ) <= yl4) and
-    (yl4 <= (ypr2 + 1 { погрешность } )) and ((xl4 = xpr1)or ((xpr1 < xl4) and (xl4 < xpr2)) )) then
-  begin
-    if FLAG then
-    begin
-      S := 'T1';
-      Image1.Canvas.MoveTo(xl4, yl4);
-      FLAG := false;
-    end
-    else if flag2 then
-    begin
-      S2 := 'T1';
-      flag2 := false;
-      Image1.Canvas.LineTo(xl4, yl4);
-    end;
-  end;
-
-  if S[1] = 'T' then
-    if S[2] = '1' then
-
-
-
-
-      // начальные точки отрезка конец
-
-
-      // конечные точки отр нач
-
-
-
-      // конечные точки отр кон
-
-      // Рисуем отрезок
-      // Image1.Canvas.LineTo(xpr2, ypr2);
-
-      // stop := false;
-      // repeat
-      // p1 := kod(x1, y1, xpr1, ypr1, xpr2, ypr2);
-      // // Присваивание кода точке с координатами (x,y)
-      // p2 := kod(x2, y2, xpr1, ypr1, xpr2, ypr2); // -//-
-      // if (not((p1 and p2) <> 0)) then // Если отрезок полностью невидим
-      // begin
-      // if ((p1 = 0) and (p2 = 0)) then // Если отрезок видим полностью
-      // begin // то рисуем его (проводим линию от точки с координатами (х1,y1) до точки - (x2,y2)
-      // Image1.Canvas.FillRect(Image1.ClientRect);
-      // Image1.Canvas.Rectangle(xpr1, ypr1, xpr2, ypr2);
-      // Image1.Canvas.MoveTo(x1, y1);
-      // Image1.Canvas.LineTo(x2, y2);
-      // exit;
-      // end
-      // else
-      // begin
-      // if p1 = 0 then
-      // // Если р1=0 - поменять местами точки (x1,y1) и (x2,y2),чтобы обработка шла каждый раз с точки, находящейся вне окна
-      // begin
-      // x0 := x1;
-      // x1 := x2;
-      // x2 := x0;
-      // y0 := y1;
-      // y1 := y2;
-      // y2 := y0;
-      // p1 := p2;
-      // end;
-      // if x1 < xpr1 then
-      // // Если точка (x1,y1) - слева от окна - найти ее новые координаты по фор-мулам
-      // begin
-      // y1 := y1 + (y2 - y1) * (xpr1 - x1) div (x2 - x1);
-      // x1 := xpr1;
-      // end
-      // else if y1 < ypr1 then
-      // // Если точка (x1,y1) - выше окна - найти ее новые координаты по формулам
-      // begin
-      // x1 := x1 + (x2 - x1) * (ypr1 - y1) div (y2 - y1);
-      // y1 := ypr1;
-      // end
-      // else if x1 > xpr2 then
-      // // Если точка (x1,y1)- справа от окна - найти ее новые координаты по формулам
-      // begin
-      // y1 := y1 + (y2 - y1) * (xpr2 - x1) div (x2 - x1);
-      // x1 := xpr2;
-      // end
-      // else if y1 > ypr2 then
-      // begin
-      // x1 := x1 + (x2 - x1) * (ypr2 - y1) div (y2 - y1);
-      // y1 := ypr2;
-      // end;
-      // end;
-      // end
-      // else
-      // begin
-      // Image1.Canvas.FillRect(Image1.ClientRect);
-      // Image1.Canvas.Rectangle(xpr1, ypr1, xpr2, ypr2);
-      // exit;
-      // end;
-      // until stop;
 end;
 
 procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton;
